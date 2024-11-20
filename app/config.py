7e1425e2a62c8480ed54848c6451cceebcd4938d
Bbabatunde123 <157145204+Bbabatunde123@users.noreply.github.com>
2024-11-20 10:20:19 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config: 
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = Project123!!!567
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.ygohuwhnlzrilptpzlps:[Project123!!!567]@aws-0-eu-central-1.pooler.supabase.com:6543/postgres
     SQLALCHEMY_TRACK_MODIFICATIONS = False
