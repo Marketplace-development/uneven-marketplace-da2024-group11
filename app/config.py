7b752c411e26ae201ed3db65b3c0937afeca1529
Bbabatunde123 <157145204+Bbabatunde123@users.noreply.github.com>
2024-11-26 19:47:46 +0100

    test

--- app/config.py
+++ app/config.py
@@ -1,9 +1,5 @@
 class Config: 
 <<<<<<< HEAD
-    SECRET_KEY = Project123!!!567
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.ygohuwhnlzrilptpzlps:[Project123!!!567]@aws-0-eu-central-1.pooler.supabase.com:6543/postgres
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-=======
     SECRET_KEY = 'c6ef4f83f71dfde80d3dcb650230795f'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.ygohuwhnlzrilptpzlps:Project123!!!567@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
