b03646f747d36a0a16cc7f30ff1ef03d2500ccd6
Glenn-Nevens <glenn.nevens@icloud.com>
2024-11-26 21:15:55 +0100

    PULL DEZE

--- app/config.py
+++ app/config.py
@@ -2,4 +2,3 @@ class Config:
     SECRET_KEY = 'c6ef4f83f71dfde80d3dcb650230795f'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.ygohuwhnlzrilptpzlps:Project123!!!567@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
